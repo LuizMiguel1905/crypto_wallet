1265e66ef1af8c37fea5117666ab3b55a3a6efb4
Luiz Miguel <luizmgc1930@gmail.com>
2024-02-16 16:43:12 -0300

    finalizacao da primeira task

--- app/javascript/controllers/previews_controller.js
+++ app/javascript/controllers/previews_controller.js
@@ -1,8 +1,27 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="previews"
+// Conecta-se a data-controller="previews"
 export default class extends Controller {
+  static targets = ["input", "preview"];
+
   connect() {
-    console.log("Hello World")
+    console.log("Hello World", this.element);
+  }
+
+  preview() {
+    let input = this.inputTarget;
+    let preview = this.previewTarget;
+    let file = input.files[0];
+    let reader = new FileReader();
+
+    reader.onloadend = () => {
+      preview.src = reader.result;
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
   }
 }
